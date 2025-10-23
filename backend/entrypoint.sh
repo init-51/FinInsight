0b92dc8dd89b2e485b1171301e324f117f40b436
init-51 <shailesh.125847@gmail.com>
2025-10-23 23:14:28 +0530

    Run Celery worker alongside API

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -e
+
 alembic upgrade head
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+
+celery -A app.celery_app worker --loglevel=info -Q backtest &
+CELERY_PID=$!
+
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+UVICORN_PID=$!
+
+trap "kill $CELERY_PID $UVICORN_PID" TERM INT
+
+wait $UVICORN_PID
 
